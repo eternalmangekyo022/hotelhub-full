ef7054aa98c2f3712fde731a9b8b04b31b44aa0a
Tibor Fazekas <tibcso0322@gmail.com>
2025-02-04 14:12:01 +0100

    added animation

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import './styles/login.scss'
 import { Link } from 'react-router-dom';
+import './styles/login.scss'
 
 interface IFormData {
 	firstname: string;
@@ -26,22 +26,23 @@ export default function Login({ register }: IProps) {
 	return <div className="login-wrapper">
 		<div className="login-form-wrapper">
 			<form className="login-form">
-				<h1>{register ? 'Register' : 'Welcome back'}</h1>
-				<h2>{register ? 'Create a HotelHub account': 'Sign in to your HotelHub account'}</h2>
-				<label htmlFor="login-email">Email</label>
-				<input type="text" name='login-email' value={formData.email} onChange={e => setFormData({ ...formData, email: e.target.value })}/>
-				<div className="password-section">
-					<label htmlFor="login-password">Password</label>
-					<span>Forgot your password?</span>
+				<div className="login-form-inner">
+					<h1>{register ? 'Register' : 'Welcome back'}</h1>
+					<h2>{register ? 'Create a HotelHub account': 'Sign in to your HotelHub account'}</h2>
+					<div className="input-wrapper">
+						<label htmlFor="login-email">Email</label>
+						<input type="text" name='login-email' value={formData.email} onChange={e => setFormData({ ...formData, email: e.target.value })}/>
+					</div>
+					<div className="input-wrapper">
+						<label htmlFor="login-password">Password</label>
+						<input type="password" name='login-password' value={formData.password} onChange={e => setFormData({ ...formData, password: e.target.value })} />
+					</div>
+					<div className="forgot-password">
+						<span>Forgot your password?</span>
+					</div>
+					<button onClick={() => {}} type='button'>{register ? 'Register' : 'Login'}</button>
+					<span>{register ? 'Already have an account?': "Don't have an account?"} <Link to={`/${register ? 'login': 'register'}`}>{register ? 'Login': 'Sign up'}</Link></span>
 				</div>
-				<input type="password" name='login-password' value={formData.password} onChange={e => setFormData({ ...formData, password: e.target.value })} />
-				<button onClick={() => {}} type='button'>{register ? 'Register' : 'Login'}</button>
-				<div className="login-divider">
-					<div className="line" />
-					<span>Or continue with</span>
-					<div className="line" />
-				</div>
-				<span>{register ? 'Already have an account?': "Don't have an account?"} <Link to={`/${register ? 'login': 'register'}`}>{register ? 'Login': 'Sign up'}</Link></span>
 			</form>
 			<div className="login-image" />
 		</div>
