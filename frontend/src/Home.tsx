fb2adb14efb57045288c513f7f759492b9fb85c5
Tibor Fazekas <tibcso0322@gmail.com>
2025-02-05 09:37:21 +0100

    login working but simple

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -2,7 +2,6 @@
 import Hero from './components/Hero.tsx'
 import Featured from './components/Featured.tsx'
 import Experiences from './components/Experiences.tsx'
-import Find from './components/Find.tsx'
 
 export default function Home() {
 	return <>
