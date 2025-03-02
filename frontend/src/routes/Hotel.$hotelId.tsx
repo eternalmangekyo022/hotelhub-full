b9eb7b431254942e20030dd1220b318d79dc995a
Tibor Fazekas <tibcso0322@gmail.com>
2025-03-02 20:18:05 +0100

    made some changes so the form can be more responsive, not collapse, and work as intended

--- frontend/src/routes/Hotel.$hotelId.tsx
+++ frontend/src/routes/Hotel.$hotelId.tsx
@@ -1,60 +1,60 @@
-import { useState } from "react";
-import { createFileRoute } from "@tanstack/react-router";
-import { useAtom } from "jotai";
-import { favoritesAtom, selectedHotelAtom } from "../store.ts";
-import "../routes/styles/details.scss";
-import star from "../assets/images/star.png";
-import emptyStar from "../assets/images/empty_star.png";
+import { useState } from 'react'
+import { createFileRoute } from '@tanstack/react-router'
+import { useAtom } from 'jotai'
+import { favoritesAtom, selectedHotelAtom } from '../store.ts'
+import '../routes/styles/details.scss'
+import star from '../assets/images/star.png'
+import emptyStar from '../assets/images/empty_star.png'
 
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
-export const Route = createFileRoute("/hotel/$hotelId")({
+export const Route = createFileRoute('/Hotel/$hotelId')({
   component: HotelDetails,
-});
+})
 
 function HotelDetails() {
-  const { hotelId } = Route.useParams();
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [isFavorite, setIsFavorite] = useState(false);
-  const [favorites, setFavorites] = useAtom(favoritesAtom);
-  const [selectedHotel] = useAtom(selectedHotelAtom);
+  const { hotelId } = Route.useParams()
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+  const [isFavorite, setIsFavorite] = useState(false)
+  const [favorites, setFavorites] = useAtom(favoritesAtom)
+  const [selectedHotel] = useAtom(selectedHotelAtom)
 
   const { data: hotel } = useQuery({
-    queryKey: ["hotel", hotelId],
+    queryKey: ['hotel', hotelId],
     queryFn: async () => {
       const { data } = await axios.get<Hotel>(
-        `http://localhost:3000/api/v1/hotels/id/${hotelId}`
-      );
-      return data;
+        `http://localhost:3000/api/v1/hotels/id/${hotelId}`,
+      )
+      return data
     },
     enabled: !selectedHotel,
     initialData: selectedHotel,
-  });
+  })
 
   function handlePrevImage() {
     if (hotel && hotel.images.length > 0) {
       setCurrentImageIndex((prevIndex) =>
-        prevIndex === 0 ? hotel.images.length - 1 : prevIndex - 1
-      );
+        prevIndex === 0 ? hotel.images.length - 1 : prevIndex - 1,
+      )
     }
   }
 
   function handleNextImage() {
     if (hotel && hotel.images.length > 0) {
       setCurrentImageIndex((prevIndex) =>
-        prevIndex === hotel.images.length - 1 ? 0 : prevIndex + 1
-      );
+        prevIndex === hotel.images.length - 1 ? 0 : prevIndex + 1,
+      )
     }
   }
 
-  const roundedRating = Math.round(hotel?.averageRating || 0);
-  const totalStars = 5;
+  const roundedRating = Math.round(hotel?.averageRating || 0)
+  const totalStars = 5
   const stars = Array.from({ length: totalStars }, (_, index) =>
-    index < roundedRating ? star : emptyStar
-  );
+    index < roundedRating ? star : emptyStar,
+  )
 
-  if (!hotel) return <div className="loading"></div>;
+  if (!hotel) return <div className="loading"></div>
 
   return (
     <div className="hotel-details">
@@ -72,7 +72,7 @@ function HotelDetails() {
           className="hotel-image"
         />
         <img
-          src={"https://www.svgrepo.com/show/440707/action-paging-prev.svg"}
+          src={'https://www.svgrepo.com/show/440707/action-paging-prev.svg'}
           alt="Next"
           className="other-arrow"
           onClick={handleNextImage}
@@ -99,11 +99,11 @@ function HotelDetails() {
             <img
               key={index}
               src={star}
-              alt={index < roundedRating ? "star" : "empty star"}
+              alt={index < roundedRating ? 'star' : 'empty star'}
             />
           ))}
         </span>
-        <span style={{ margin: ".2rem" }}>{`(${hotel.ratingCount || 0})`}</span>
+        <span style={{ margin: '.2rem' }}>{`(${hotel.ratingCount || 0})`}</span>
       </p>
       <button
         className="book-now-btn"
@@ -112,7 +112,7 @@ function HotelDetails() {
         Add to Favorites
       </button>
     </div>
-  );
+  )
 }
 
-export default HotelDetails;
+export default HotelDetails
