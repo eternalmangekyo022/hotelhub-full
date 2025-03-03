5255d9435cec4c65a1e42e54a935abe7c7a39ef5
Tibor Fazekas <tibcso0322@gmail.com>
2025-03-03 08:40:44 +0100

    na

--- frontend/src/components/Find.tsx
+++ frontend/src/components/Find.tsx
@@ -15,10 +15,9 @@ export default function Find() {
   const [location, setLocation] = useState("");
   const [name, setName] = useState("");
   const [isSimple, setIsSimple] = useState(true);
-  const [selectedSort, setSelectedSort] = useState(""); // State to track selected sorting option
   const [, setSearchQuery] = useAtom(searchQueryAtom);
 
-  const [, setSortBy] = useAtom(sortByAtom);
+  const [sortBy, setSortBy] = useAtom(sortByAtom);
 
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
@@ -27,17 +26,21 @@ export default function Find() {
 
   const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const sortBySchema = z.enum([
+      "",
       "name-asc",
       "name-desc",
       "location-asc",
       "location-desc",
       "rating-asc",
       "rating-desc",
+      "ratingtotal-asc",
+      "ratingtotal-desc",
+      "price-asc",
+      "price-desc",
     ]);
     if (!sortBySchema.safeParse(e.target.value).success) return;
-    const sortBy = e.target.value as ISortBy;
-    setSelectedSort(sortBy);
-    setSortBy(sortBy); // Pass the selected sorting option to the parent
+    const toSortBy = e.target.value as ISortBy;
+    setSortBy(toSortBy);
   };
 
   return (
@@ -116,7 +119,7 @@ export default function Find() {
         {/* Sort By Section */}
         <div className="sort-by">
           <label htmlFor="sort">Sort By:</label>
-          <select id="sort" value={selectedSort} onChange={handleSortChange}>
+          <select id="sort" value={sortBy} onChange={handleSortChange}>
             <option value="">None</option>
             <option value="name-asc">Alphabetical (A-Z)</option>
             <option value="name-desc">Alphabetical (Z-A)</option>
