9a89936c87f354d34a4b08a7bf2834fb43fc8f9c
szabi <szabikun1@gmail.com>
2025-03-14 15:54:37 +0100

    name change

--- frontend/src/routes/booking/$id.tsx
+++ frontend/src/routes/booking/$id.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import HotelBooking  from '../Booking'
 
-export const Route = createFileRoute('/bookings/$id')({
+export const Route = createFileRoute('/booking/$id')({
   component: HotelBooking,
 })
 
